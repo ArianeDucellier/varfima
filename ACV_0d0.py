f8c09b1083c68a5acb019759961e28a2eea8a7cb
ArianeDucellier <ariane.ducellier@gmail.com>
2023-05-23 09:31:18 -0700

    add ACV of VARFIMA(0,d,q)

--- ACV_0d0.py
+++ ACV_0d0.py
@@ -1,41 +1,39 @@
 """
 This module compute the autocovariance of a VARFIMA(0, d, 0) model
 """
-import numpy as np
-
-from scipy.special import gamma
+import torch
 
 def compute_next(omega_old, d, h):
     """
     Compute the autocovariance matrix at lag h
     from the autocovariance matrix at lag h-1
     Input:
-      omega_old = 2D numpy array, autocovariance matrix at lag h - 1
-      d = 1D numpy array, orders of the fractionally integrated process
-      h = scalar, lag time at which we compute the autocovariance matrix
+      omega_old = 2D array, autocovariance matrix at lag h - 1
+      d = 1D array, orders of the fractionally integrated process
+      h = integer, lag time at which we compute the autocovariance matrix
     Output:
-      omega = 2D numpy array, autocovariance matrix at lag h
+      omega = 2D array, autocovariance matrix at lag h
     """
-    assert isinstance(omega, np.ndarray), \
-        'The autocovariance at lag h - 1 should be a numpy array'
-    assert len(np.shape(omega_old)) == 2, \
+    assert isinstance(omega_old, torch.Tensor), \
+        'The autocovariance at lag h - 1 should be a torch tensor'
+    assert len(omega_old.size()) == 2, \
         'The dimension of the autocovariance at lag h - 1 should be 2'
-    assert np.shape(omega_old)[0] == np.shape(omega_old)[1], \
+    assert omega_old.size()[0] == omega_old.size()[1], \
         'The autocovariance at lag h - 1 should be a square matrix'
-    assert isinstance(d, np.ndarray), \
-        'The orders of the fractionally integrated process should be a numpy array'
-    assert len(np.shape(d)) == 1, \
+    assert isinstance(d, torch.Tensor), \
+        'The orders of the fractionally integrated process should be a torch Tensor'
+    assert len(d.size()) == 1, \
         'The dimension of the orders of the fractionally integrated process should be 1'
-    assert np.shape(omega_old)[0] == np.shape(d)[0], \
+    assert omega_old.size()[0] == d.size()[0], \
         'The length of the orders of the fractionally integrated process should be ' + \
         'equal to the dimension of the autocovariance matrix at lag h - 1'
-    assert isinstance(h, in), \
+    assert isinstance(h, int), \
         'The lag of the autocovariance should be an integer'
     assert h >= 1, \
         'The lag should be higher or equal to 1'
 
-    r = np.shape(d)[0]
-    omega = np.copy(omeda_old)
+    r = d.size()[0]
+    omega = torch.FloatTensor(r, r)
     for m in range(0, r):
         for n in range(0, r):
             omega[m, n] = omega_old[m, n] * (h - 1 + d[n]) / (h - d[m])
@@ -45,36 +43,36 @@ def compute_autocovariance(sigma, d, h):
     """
     Compute the autocovariance matrices up to lag h
     Input:
-      sigma = 2D numpy array, covariance of the white noise process
-      d = 1D numpy array, orders of the fractionally integrated process
-      h = scalar, lag time up to which we compute the autocovariance matrix
+      sigma = 2D array, covariance of the white noise process
+      d = 1D array, orders of the fractionally integrated process
+      h = integer, lag time up to which we compute the autocovariance matrix
     Output:
-      omega = 3D numpy array, autocovariance matrix at lag 0, 1, ... , h
+      omega = 3D array, autocovariance matrix at lag 0, 1, ... , h
     """
-    assert isinstance(sigma, np.ndarray), \
-        'The covariance of the white noise process should be a numpy array'
-    assert len(np.shape(sigma)) == 2, \
+    assert isinstance(sigma, torch.Tensor), \
+        'The covariance of the white noise process should be a torch tensor'
+    assert len(sigma.size()) == 2, \
         'The dimension of the covariance of the white noise process should be 2'
-    assert np.shape(sigma)[0] == np.shape(sigma)[1], \
+    assert sigma.size()[0] == sigma.size()[1], \
         'The covariance of the white noise process should be a square matrix'
-    assert isinstance(d, np.ndarray), \
-        'The orders of the fractionally integrated process should be a numpy array'
-    assert len(np.shape(d)) == 1, \
+    assert isinstance(d, torch.Tensor), \
+        'The orders of the fractionally integrated process should be a torch tensor'
+    assert len(d.size()) == 1, \
         'The dimension of the orders of the fractionally integrated process should be 1'
-    assert np.shape(sigma)[0] == np.shape(d)[0], \
+    assert sigma.size()[0] == d.size()[0], \
         'The length of the orders of the fractionally integrated process should be ' + \
         'equal to the dimension of the covariance of the white noise process'
-    assert isinstance(h, in), \
+    assert isinstance(h, int), \
         'The maximum lag of the autocovariance should be an integer'
     assert h >= 1, \
         'The maximum lag should be higher or equal to 1'
 
-    r = np.shape(d)[0]
-    omega = np.zeros((r, r, h + 1))
+    r = d.size()[0]
+    omega = torch.FloatTensor(r, r, h + 1)
     for m in range(0, r):
         for n in range(0, r):
-            omega[m, n, 0] = sigma[m, n] * gamma(1 - d[m] - d[n]) \
-                / (gamma(1 - d[m]) * gamma(1 - d[n]))
+            omega[m, n, 0] = sigma[m, n] * torch.exp(torch.lgamma(1 - d[m] - d[n])) \
+                / (torch.exp(torch.lgamma(1 - d[m])) * torch.exp(torch.lgamma(1 - d[n])))
     for k in range(1, h + 1):
         omega[:, :, k] = compute_next(omega[:, :, k - 1], d, h)
     return omega
---
+++ ACV_0dq.py
@@ -0,0 +1,96 @@
+"""
+This module compute the autocovariance of a VARFIMA(0, d, q) model
+"""
+import torch
+
+import ACV_0d0
+
+def compute_gamma_function(d, h, q):
+    """
+    Compute an auxiliary function containing the ratio
+    of gamma functions of d, h and l with -q <= l <= q
+    Input:
+      d = 1D array, orders of the fractionally integrated process
+      h = integer, lag time at which we compute the autocovariance matrix
+      q = integer, order of the MA part of the fractionally integrated process
+    Output:
+      gamma_f = 3D array (r * r * 2q+1) where r is the length of d
+    """
+    assert isinstance(d, torch.Tensor), \
+        'The orders of the fractionally integrated process should be a torch tensor'
+    assert len(d.size()) == 1, \
+        'The dimension of the orders of the fractionally integrated process should be 1'
+    assert isinstance(h, int), \
+        'The lag of the autocovariance should be an integer'
+    assert h >= 1, \
+        'The lag should be higher or equal to 1'
+    assert isinstance(q, int), \
+        'The order of the MA part of the fractionally integrated process should be an integer'
+    assert h >= 1, \
+        'The order of the MA part of the fractionally integrated process should be higher or equal to 1'
+
+    r = d.size()[0]
+    gamma_f = torch.zeros(r, r, 2 * q + 1)
+    gamma_f[:, :, q] = torch.ones((r, r)) # gamma_f(0)
+    for m in range(0, r):
+        for n in range(0, r):
+            for k in range(1, q + 1):
+                # gamma_f(-k)
+                gamma_f[m, n, q - k] = gamma_f[m, n, q - k + 1] * (h - d[m] - k + 1) / (h + d[n] - k)
+                # gamma_f(k)
+                gamma_f[m, n, q + k] = gamma_f[m, n, q + k] * (h + d[n] + k - 1) / (h - d[m] + k)
+    return gamma_f
+
+def compute_autocovariance(sigma, theta, d, h):
+    """
+    Compute the autocovariance matrices up to lag h
+    Input:
+      sigma = 2D array, covariance of the white noise process
+      d = 1D array, orders of the fractionally integrated process
+      h = integer, lag time up to which we compute the autocovariance matrix
+    Output:
+      omega = 3D array, autocovariance matrix at lag 0, 1, ... , h
+    """
+    assert isinstance(sigma, torch.Tensor), \
+        'The covariance of the white noise process should be a torch tensor'
+    assert len(sigma.size()) == 2, \
+        'The dimension of the covariance of the white noise process should be 2'
+    assert sigma.size()[0] == sigma.size()[1], \
+        'The covariance of the white noise process should be a square matrix'
+    assert isinstance(d, torch.Tensor), \
+        'The orders of the fractionally integrated process should be a torch tensor'
+    assert len(d.size()) == 1, \
+        'The dimension of the orders of the fractionally integrated process should be 1'
+    assert sigma.size()[0] == d.size()[0], \
+        'The length of the orders of the fractionally integrated process should be ' + \
+        'equal to the dimension of the covariance of the white noise process'
+    assert isinstance(h, int), \
+        'The maximum lag of the autocovariance should be an integer'
+    assert h >= 1, \
+        'The maximum lag should be higher or equal to 1'
+
+    r = d.size()[0]
+    q = theta.size()[2]
+    omega = np.zeros((r, r, h + 1))
+    
+    omega_star = ACV_0d0.compute_autocovariance(torch.eye(r), d, h)
+
+    f0 = torch.matmul(torch.matmul(torch.diag(torch.diagonal(theta[:, :, 0])), sigma), \
+                                   torch.diag(torch.diagonal(theta[:, :, 0])))
+
+    for k in range(0, h + 1):
+        gamma_f = compute_gamma_function(d, k, q)
+    
+        f1 = torch.zeros(r, r)
+        f2 = torch.zeros(r, r)
+        f3 = torch.zeros(r, r)
+        for f in range(1, q + 1):
+            for g in range(1, q + 1):
+                f1 = f1 + torch.matmul(torch.matmul(theta[:, :, f], sigma), \
+                                    torch.transpose(theta[:, :, g])) * gamma_f[:, :, q + g - f]
+            f2 = f2 + torch.matmul(theta[:, :, f], torch.transpose(sigma)) * gamma_f[q - f]
+            f3 = f3 + torch.matmul(sigma, torch.transpose(theta[:, :, f])) * gamma_f[q + f]
+    
+        omega[:, :, k] = omega_star * (f0 + f1 + f2 + f3)
+
+    return omega
